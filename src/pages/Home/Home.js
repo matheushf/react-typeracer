387c6b02130e4a6a0a02ba24b5180ced0b444277
Matheus Victor <hffmatheus@gmail.com>
2019-04-19 23:34:56 -0300

    remove unused files and eslint fixes

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import logo from '../../logo.svg';
 import './Home.css';
 import '../../App.css';
